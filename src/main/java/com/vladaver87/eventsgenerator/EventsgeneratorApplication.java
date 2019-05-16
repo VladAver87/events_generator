9101460fbe1cff9c17c0f451567dcb60357732a7
Your NameVladAver87 <Averianov87@gmail.com>
2019-05-16 11:52:34 +0300

    creating project

---
+++ src/main/java/com/vladaver87/eventsgenerator/EventsgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.vladaver87.eventsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsgeneratorApplication.class, args);
+	}
+
+}
