bb70510295d98a90f9beb4358dec2e50d8b68ee2
Your NameVladAver87 <Averianov87@gmail.com>
2019-05-29 15:34:05 +0300

    add event stage end

--- src/main/java/com/vladaver87/eventsgenerator/generator/EventJoiner.java
+++ src/main/java/com/vladaver87/eventsgenerator/generator/EventJoiner.java
@@ -1,26 +1,44 @@
 package com.vladaver87.eventsgenerator.generator;
 
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 import com.vladaver87.eventsgenerator.model.Event;
 
 @Component
 public class EventJoiner {
-	
-	private static final Logger log = LoggerFactory.getLogger(EventJoiner.class);
-	
+
 	public void joining(Event e) {
 		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		service.schedule(new EventJoinJob(e), new Random().nextInt(11), TimeUnit.SECONDS);
+		service.schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				e.setEventType("join");
+				e.setDeliveryTime(new Date());
+				e.setAgentId("Agent_Id_" + new Random().nextInt(17));			
+			}
+			
+		}, new Random().nextInt(11), TimeUnit.SECONDS);
+		
+		
+		if(!service.isTerminated()) {
+			service.schedule(new Runnable() {
+
+				@Override
+				public void run() {
+					e.setEventType("end");
+					e.setEndTime(new Date());
+					e.setEndReason("NORMAL");			
+				}
+				
+			}, new Random().nextInt(25), TimeUnit.SECONDS);
+			
+		}
 		service.shutdown();		
-		log.info("Event move to join status " + e.toString());	
 	}
 
 }
